132e1da73ddcfe7c02a796e8dc1e6ed839a88347
diasssss999 <diasssss999@gmail.com>
2025-11-24 13:36:00 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// File konfigurasi untuk API Key
+// JANGAN di-commit ke GitHub atau repository public!
+
+const API_KEY = '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';
